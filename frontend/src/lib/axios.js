9df73c71a054f800607fcebd77a2b43218f30576
BHUMIKA TYAGI <tyagi.bhumika24@email.com>
2025-06-26 14:19:25 +0530

    Project Completed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,4 +5,3 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 })
-export default axiosInstance;
