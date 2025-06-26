a0c17ed4aa035cd30a41719ec2665e05e420c7c4
BHUMIKA TYAGI <tyagi.bhumika24@email.com>
2025-06-26 12:38:34 +0530

    💡 Update: Made some changes in frontend/backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,38 +1,32 @@
-import { connectDB } from "./lib/db.js";
-
-import dotenv from "dotenv";
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
-import cookieParser from "cookie-parser";
-import { app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
-
-// Connect to DB
-connectDB();
-console.log("MONGO_URL from env:", process.env.MONGO_URL);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.get("/", (req, res) => {
-  res.send("Chat backend is running!");
-});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -43,5 +37,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(PORT, () => {
-  console.log("Server is running on PORT:", PORT);
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
